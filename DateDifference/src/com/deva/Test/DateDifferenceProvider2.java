b3d2134bcd2292529945011ed1dffaca2cf9bb3e
164388_deva <32515992+164388deva@users.noreply.github.com>
2018-11-20 18:47:52 +0530

    Add files via upload

---
+++ DateDifference/src/com/deva/Test/DateDifferenceProvider2.java
@@ -0,0 +1,20 @@
+package com.deva.Test;
+
+import com.deva.supp.MyDate;
+
+public class DateDifferenceProvider2 {
+	static final int leapYear[]={0,31,29,31,30,31,30,31,31,30,31,30,31};
+	static final int nonLeapYear[]={0,31,28,31,30,31,30,31,31,30,31,30,31};
+	
+	
+	public   int differeceCalculator(MyDate startDate,MyDate endDate){
+		
+		
+		
+		
+		
+		return 0;
+
+	
+	}
+}
